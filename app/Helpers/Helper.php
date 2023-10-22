887691f7f8bc555fb039a1d23321e6d8d813a98d
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-10-22 09:49:33 +0530

    small updates

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -27,16 +27,16 @@ function checkStockExists($request, $id)
         return $q->where('type_id', $request->type_id);
     })->when($request->material_id != '', function ($q) use ($request) {
         return $q->where('material_id', $request->material_id);
+    })->when($request->axis != '', function ($q) use ($axis) {
+        return $q->whereIn('axis', $axis);
+    })->when($request->add != '', function ($q) use ($add) {
+        return $q->whereIn('add', $add);
     })->when($request->sph != '' && $request->cyl != '', function ($q) use ($spherical, $cylinder) {
         return $q->whereRaw("IF($spherical, CAST($spherical AS DECIMAL(4,2)) = CAST(sph AS DECIMAL(4,2))+CAST(cyl AS DECIMAL(4,2)), 1)")->whereRaw("IF($cylinder, CAST($cylinder AS DECIMAL(4,2)) = CAST(0-cyl AS DECIMAL(4,2)), 1)")->orWhereRaw("sph=$spherical AND cyl=$cylinder");
     })->when($request->sph != '' && $request->cyl == '', function ($q) use ($sph) {
         return $q->whereIn('sph', $sph)->whereNull('cyl')->orwhere('cyl', 0);
     })->when($request->sph == '' && $request->cyl != '', function ($q) use ($cyl, $cylinder) {
         return $q->whereIn('cyl', $cyl)->whereNull('sph')->orWhere('sph', number_format(0 - $cylinder, 2))->orWhere('sph', 0);
-    })->when($request->axis != '', function ($q) use ($axis) {
-        return $q->whereIn('axis', $axis);
-    })->when($request->add != '' && $request->add > 0, function ($q) use ($add) {
-        return $q->whereIn('add', $add);
     })->orderByDesc('add')->get();
     return ($id > 0 && count($products) == 1) ? collect() : $products;
 }
